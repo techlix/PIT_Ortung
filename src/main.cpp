21b345d923e757943b14c81648328c48755db12d
techlix <55599305+techlix@users.noreply.github.com>
2019-09-20 21:18:57 +0200

    LEDtest

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(GPIO_NUM_13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(GPIO_NUM_13, HIGH);
+  delay(20);
+  digitalWrite(GPIO_NUM_13, LOW);
+  delay(20);
+
+}
