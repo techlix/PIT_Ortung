5981ec119f017adecd50e0d0f0e3e1fdf83e4606
Leonard-PIT <55599133+Leonard-pit@users.noreply.github.com>
2019-09-21 11:14:10 +0200

    Display

--- Display/src/main.cpp
+++ Display/src/main.cpp
@@ -7,6 +7,10 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
 unsigned long drawTime = 0;
 
+int distance1;
+int distance2;
+int distance3;
+
 void setup(void) {
   tft.init();
   tft.setRotation(1);
@@ -18,9 +22,20 @@ tft.fillScreen(TFT_PINK);
 
 
   tft.setTextColor(TFT_RED, TFT_BLUE);
-  i++;
-  tft.setCursor(i,i);
-  tft.print("Hello World!");
+  tft.drawString("Beacon 1",0,0);
+  char *intStr = itoa(distance1);
+  String strDOne = string(intStr);
+  tft.text(strDOne,0,10)
+  tft.drawString("Beacon 2",0,50);
+  char *intStrI = itoa(distance2);
+  String strDTwo = string(intStrI);
+  tft.text(strDTwo,0,60);
+  tft.drawString("Beacon 3", 0,150);
+  char *intStrII = itoa(distance3);
+  string strDThree = string(intStrII);
+  tft.text(strDTHree,0,60);
+
+  
 
   
   delay(100);
